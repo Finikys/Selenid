bb937a9401a743d9cf8205f3738c9aafa66686a6
Finikys <ForOnceFinikys@gmail.com>
2023-03-14 21:42:26 +0300

    Edit RegistrationTest.java

--- src/test/java/ru/netology/web/RegistrationTest.java
+++ src/test/java/ru/netology/web/RegistrationTest.java
@@ -1,28 +1,38 @@
 package ru.netology.web;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 class RegistrationTest {
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
     @Test
     void shouldRegisterByAccountNumberDOMModification() {
+        String planningDate = generateDate(4);
         open("http://localhost:9999");
         $("[placeholder='Город']").setValue("Волгоград");
         $("[placeholder='Дата встречи']").sendKeys(Keys.CONTROL + "a");
         $("[placeholder='Дата встречи']").sendKeys(Keys.BACK_SPACE);
-        $("[placeholder='Дата встречи']").setValue("31032023");
+        $("[placeholder='Дата встречи']").setValue(planningDate);
         $("[name='name']").setValue("Бирюков Александр");
         $("[name='phone']").setValue("+79999999999");
         $("[class='checkbox__box']").click();
         $("[class='button__text']").click();
         $("[class='notification__title']").shouldBe(visible, Duration.ofSeconds(12));
+        $(".notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15)).shouldBe(Condition.visible);
     }
 }
 
