c7fafb394b8ae0e2e74b6738478136e8c883cc76
Finikys <ForOnceFinikys@gmail.com>
2023-03-14 18:09:06 +0300

    Initial commit

---
+++ src/test/java/ru/netology/web/RegistrationTest.java
@@ -0,0 +1,23 @@
+package ru.netology.web;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+class RegistrationTest {
+    @Test
+    void shouldRegisterByAccountNumberDOMModification() {
+        open("http://localhost:9999");
+        $$(".tab-item").find(exactText("По номеру счёта")).click();
+        $("[name='number']").setValue("4055 0100 0123 4613 8564");
+        $("[name='phone']").setValue("+792000000000");
+        $$("button").find(exactText("Продолжить")).click();
+        $(withText("Успешная авторизация")).shouldBe(visible, Duration.ofMillis(5000));
+        $(byText("Личный кабинет")).shouldBe(visible, Duration.ofMillis(5000));
+    }
+}
+
